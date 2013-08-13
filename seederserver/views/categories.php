fdd6d2b7179e87702c33dec6d9bb4d72ea8c1a14
ystallonne <ystallonne@gmail.com>
2013-08-13 18:55:35 -0400

    Update categories.php
    Retrieving all games and adding them in a select

--- seederserver/views/categories.php
+++ seederserver/views/categories.php
@@ -1,10 +1,29 @@
+<!doctype html>
 <html>
-    <head></head>
-    <body>
-        <h1>Welcome to Our Website!</h1>
-        <hr/>
-        <h2>Categories</h2>
-        <h4><?=$data['title'];?></h4>
-        <p><?=$data['content'];?></p>
-    </body>
+<head>
+<meta charset="utf-8">
+<title>Seeder | Show all games</title>
+<script src="http://code.jquery.com/jquery-latest.js"></script>
+</script>
+<script>
+$(document).ready(function(){ 
+	$.getJSON("http://seederserver.aws.af.cm/index.php?categories&command=getAllGames",function(result){
+
+		var arrayOfObjects = result;
+		
+		for (var i = 0; i < arrayOfObjects.length; i++) {
+			var object = arrayOfObjects[i];
+			$('#games').append($('<option>', {
+				value: JSON.stringify(object[0].idCategory),
+				text: JSON.stringify(object[0].name).replace(/["]/g, "")
+			}));
+		}
+	});
+});
+</script>
+</head>
+<body>
+<select id="games">
+</select>
+</body>
 </html>
