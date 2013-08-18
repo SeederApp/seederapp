dfd76ab03ce583d0a32a2bdd3fd71a13303011c0
Ystallonne Carlos da Silva Alves <ystallonne@gmail.com>
2013-08-18 18:36:52 -0400

    methods updated in comments models

--- seederserver/models/comments.php
+++ seederserver/models/comments.php
@@ -13,36 +13,70 @@ class Comments_Model {
 	}
 
 	/*
+	 * @email email
+	 */
+	private function getHashByEmail($email) {
+		//Connect to database
+		$this->db->connect();
+		
+		//Prepare query
+		$this->db->prepare("SELECT hash FROM User WHERE email = '".$email."';");
+		
+		//Execute query
+		$this->db->query();
+	
+		//Fetch query
+		$article = $this->db->fetch('array');
+		
+		//Return data
+		return $article;
+	}
+
+	/*
 	 * @params[0] email
-	 * @params[1] idIdea
-	 * @params[2] content
+	 * @hash hash sent by the client
+	 */
+	private function authenticateUser($email, $hash) {
+		$userHash = $this->getHashByEmail($email);
+		$userHashDecoded = json_decode($userHash, true);
+		if ($userHashDecoded != null) {
+			if ($userHashDecoded[0][0][0] != $hash) {
+				return false;
+			}
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	/*
+	 * @params[0] email
+	 * @params[1] idUser
+	 * @params[2] idIdea
+	 * @params[3] content
 	 * @hash hash sent by the client
 	 */
 	public function addComment($params, $hash) {
 		//Authenticate user
-		$userHash = $this->getHashByEmail($params[0]);
-		$decodedHash = json_decode($userHash, true);
-		
-		//Check if the sent client-side hash is different than the one in the database
-		if ($decodedHash[0][0][0] != $hash){
-			return;
+		if (!$this->authenticateUser($params[0], $hash)){
+			return "Invalid user or password";
 		}
 		
 		//Connect to database
 		$this->db->connect();
 		
 		//Prepare query
-		$this->db->prepare("INSERT INTO Comment (date, content) VALUES (now(), '".$params[2]."');");
+		$this->db->prepare("INSERT INTO Comment (date, content) VALUES (now(), '".$params[3]."');");
 		
 		//Execute query and return "true" or "false"
 		if ($this->db->query()){
 			//Prepare query
-			$this->db->prepare("INSERT INTO Idea_Comment (idIdea, idComment) VALUES ('".$params[1]."', '".$this->db->insert_id."');");
+			$this->db->prepare("INSERT INTO Idea_Comment (idIdea, idComment) VALUES ('".$params[2]."', '".$this->db->insert_id."');");
 			
 			//Execute query and return "true" or "false"
 			if ($this->db->query()){
 				//Prepare query
-				$this->db->prepare("INSERT INTO User_Comment (idUser, idComment) VALUES ('".$params[0]."', '".$this->db->insert_id."');");
+				$this->db->prepare("INSERT INTO User_Comment (idUser, idComment) VALUES ('".$params[1]."', '".$this->db->insert_id."');");
 				
 				//Return "true" or "false"
 				return $this->db->query();
